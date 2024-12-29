ef8ee587ea3f1b7bd47d2fdfb431857c1fd4f9ac
Michal Maršálek <MichalMarsalek@users.noreply.github.com>
2024-12-29 23:32:58 +0100

    add doc comment

--- scripts/runner-lib.ts
+++ scripts/runner-lib.ts
@@ -306,6 +306,11 @@ export function rand(a: number, b?: number): number {
   return b === undefined ? Math.floor(Math.random() * a) : rand(b - a) + a;
 }
 
+/**
+ * Shuffles an array in place, then returns it.
+ * @param array The array to shuffle
+ * @returns The same array reference
+ */
 export function shuffle<T>(array: T[]): T[] {
   for (let i = array.length - 1; i >= 0; i--) {
     const j = rand(i + 1);
