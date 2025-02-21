df775d78d28a00e910ce87480f2fa97f5b05eeec
Maurits "Mousetail" van Riezen <mousetail@mousetail.nl>
2025-02-21 15:22:05 +0100

    show on challenge page if a challenge has ended

--- main-server/src/auto_output_format/get_tera.rs
+++ main-server/src/auto_output_format/get_tera.rs
@@ -2,7 +2,9 @@ use std::{collections::HashMap, sync::OnceLock};
 
 use axum::response::Response;
 use common::langs::LANGS;
+use sqlx::types::time::OffsetDateTime;
 use tera::{to_value, Tera, Value};
+use tower_sessions::cookie::time::format_description;
 
 use crate::markdown::MarkdownFilter;
 
@@ -18,6 +20,7 @@ pub fn get_tera() -> Result<&'static Tera, Response> {
             tera.register_function("modules", load_assets);
             tera.register_filter("markdown", MarkdownFilter);
             tera.register_filter("format_number", format_number);
+            tera.register_filter("format_date", format_date);
             tera
         })
     });
@@ -47,11 +50,9 @@ fn format_number(value: &Value, data: &HashMap<String, Value>) -> Result<Value, 
     }
 
     match value {
-        Value::Number(number) => {
-            Ok(Value::String(format_number_with_thousands_seperators(
-                number.as_i64().unwrap(),
-            )))
-        }
+        Value::Number(number) => Ok(Value::String(format_number_with_thousands_seperators(
+            number.as_i64().unwrap(),
+        ))),
         _ => Err(tera::Error::msg(format!("Expected a number, got {value}"))),
     }
 }
@@ -60,4 +61,35 @@ fn format_number_with_thousands_seperators(num: i64) -> String {
     use num_format::{Locale, ToFormattedString};
 
     num.to_formatted_string(&Locale::en)
+}
+
+fn format_date(value: &Value, data: &HashMap<String, Value>) -> Result<Value, tera::Error> {
+    if !data.is_empty() {
+        return Err(tera::Error::msg(
+            "The format string filter takes no parameters",
+        ));
+    }
+    let date: OffsetDateTime = serde_json::from_value(value.clone()).map_err(tera::Error::json)?;
+
+    let offset = (date - OffsetDateTime::now_utc()).abs();
+
+    Ok(Value::String(if offset.whole_weeks() > 12 {
+        date.format(
+            &format_description::parse("[year]-[month]-[day]")
+                .map_err(|_e| tera::Error::call_filter("format_date", "unkown"))?,
+        )
+        .map_err(|_e| tera::Error::call_filter("format_date", "unkown"))?
+    } else if offset.whole_weeks() != 0 {
+        format!(
+            "{} weeks, {} days",
+            offset.whole_weeks(),
+            offset.whole_days() % 7
+        )
+    } else {
+        format!(
+            "{} days, {} hours",
+            offset.whole_days(),
+            offset.whole_hours() % 24
+        )
+    }))
 }
