c9d5ae4bf03fed39d535d69bdd7f8ec8f5d8ab6f
lyxal <36217120+lyxal@users.noreply.github.com>
2025-08-15 00:59:38 +1000

    Version bump vyxal to 3.9.0 (#108)

--- common/src/langs.rs
+++ common/src/langs.rs
@@ -110,7 +110,7 @@ pub const LANGS: phf::Map<&'static str, Lang> = phf_map! {
             ("LD_LIBRARY_PATH", "/java/lib:/lib"),
             ("JAVA_TOOL_OPTIONS", "-Dfile.encoding=UTF-8")
         ],
-        latest_version: "3.8.0",
+        latest_version: "3.9.0",
         icon: "vyxal3.svg",
         extra_mounts: &[
             ("/usr/lib/jvm/java-17-openjdk-amd64", "/java", )
