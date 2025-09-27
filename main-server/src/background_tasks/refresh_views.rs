9b284c734925140e20de77a5b7afac639875cfb3
Maurits "Mousetail" van Riezen <mousetail@mousetail.nl>
2025-09-27 12:52:07 +0200

    fix solutions count

--- main-server/src/background_tasks/refresh_views.rs
+++ main-server/src/background_tasks/refresh_views.rs
@@ -24,6 +24,16 @@ pub async fn refresh_views_task(pool: PgPool) {
             }
         }
 
+        let statement = query!("REFRESH MATERIALIZED VIEW user_scoring_info_per_language")
+            .execute(&pool)
+            .await;
+        match statement {
+            Ok(_) => (),
+            Err(e) => {
+                eprintln!("Error refreshing scores: {e:?}");
+            }
+        }
+
         tokio::time::sleep(Duration::from_secs(30)).await;
     }
 }
